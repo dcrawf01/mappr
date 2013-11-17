0c990fd57ca4d2124917311f5b62fad24190b861
Rory Crawford <rory@rory-VirtualBox.(none)>
2013-11-17 14:34:06 -0500

    Initial git commit for mappr application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mappr::Application.config.secret_key_base = '4641af85c769f711d4b8fb544902fb2af0ee2332268a85e891c8f237ef1250cb16eacc90b5013c82b31fad3bd469bdb96d95a6f8457f6cd02af5d707fcd3b7b1'
